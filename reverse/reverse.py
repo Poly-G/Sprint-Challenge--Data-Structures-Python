edf99a72d291d3270e8fb9eecd0e36a87db13459
Poly Goveia <poly.goveia@gmail.com>
2020-05-29 21:00:44 -0700

    implement reverse.py

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -39,4 +40,22 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        # set initial head to current
+        current = self.head
+        # set initial node prior to head (old) to None
+        old = None
+        # loop while current is not none (current is set to next_node each loop which eventually becomes None)
+        while current:
+            # set next node to current's next_node attribute
+            next = current.next_node
+            # set current's next_node reference to old (reversing the next reference)
+            current.next_node = old
+            # set old node reference to current
+            old = current
+            # set current node reference to next in preparation to start next loop
+            current = next
+        # reset head to the latest old which will be the very last node
+        self.head = old
+
+        # before: old => current => next_node
+        # now: next_node (old) => current => old (next_node)
